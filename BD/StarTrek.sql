12613c9fdcfced496963f5e2fbc2eed9bf16eaae
Nacho Espejo Garcia <32483835+NachoEspejo@users.noreply.github.com>
2018-06-14 10:15:16 +0200

    Update StarTrek.sql

--- BD/StarTrek.sql
+++ BD/StarTrek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-03-2018 a las 16:32:24
--- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 7.1.10
+-- Tiempo de generación: 14-06-2018 a las 10:14:02
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `StarTrek`
+-- Base de datos: `startrek`
 --
 
 -- --------------------------------------------------------
@@ -44,30 +44,30 @@ INSERT INTO `faccion` (`IdFacc`, `NombreFacc`) VALUES
 
 -- --------------------------------------------------------
 
--- 
+--
 -- Estructura de tabla para la tabla `tripulante`
--- 
+--
 
 CREATE TABLE `tripulante` (
-  `IdTrip` int(5) NOT NULL,
+  `IdTrip` int(10) NOT NULL,
   `nombre` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `apellidos` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `IdFacc` int(15) NOT NULL,
-  `Equipo` varchar(999) COLLATE utf8mb4_unicode_ci NOT NULL
+  `Historia` varchar(9999) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Equipo` varchar(999) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- 
+--
 -- Volcado de datos para la tabla `tripulante`
--- 
+--
 
-INSERT INTO `tripulante` (`IdTrip`, `nombre`, `apellidos`, `IdFacc`, `Equipo`) VALUES
-(1, 'Donelle', 'McLise', 1, 'Blaster XNV07'),
-(2, 'Carson', 'Readitt', 1, 'Gracias a sus esfuerzos y determinación, fue reconocido como unos de los mejores efectivos de la Federación', 'Blaster XNV07'),
-(3, 'Isahella', 'Hiddsley', 2, 'Primer lider de la casa Klingon',  'Blaster XNV07'),
-(4, 'Porter', 'Schoffler', 2, 'Noble de alto rango de la casa Klingon',  'Blaster XNV07'),
-(5, 'Magdalena', 'Kevlin', 3, 'Fue considerada una marginada en la Tierra, lo que provocó un fuerte odio hacia la Federación, conviertiendose en la primera rebelde y la más peligrosa',  'Blaster XNV07'),
-(6, 'Sonnnie', 'Inglish', 3, 'Es considera la mercenaria más temeraria, en toda la galaxia, ella cualquier tarea, por el precio justo.',  'Blaster XNV07');
-
+INSERT INTO `tripulante` (`IdTrip`, `nombre`, `apellidos`, `IdFacc`, `Historia`, `Equipo`) VALUES
+(1, 'Donelle', 'McLise', 1, 'Esta es algo de locos', 'Blaster XNV07'),
+(31, ' Test1', 'Test1', 1, 'Test1', 'Test1'),
+(32, ' Test2', 'Test2', 1, 'Test2', 'Test2'),
+(33, ' Test3', 'Test3', 1, 'Esto es de locos pero no hay problemas tete', 'Mag48'),
+(34, ' Test4', 'Test4', 1, 'Esto es una prueba', 'LightSaber'),
+(36, ' Prueba', 'Prueba', 1, 'Historia', 'Equipo');
 
 --
 -- Índices para tablas volcadas
@@ -76,7 +76,6 @@ INSERT INTO `tripulante` (`IdTrip`, `nombre`, `apellidos`, `IdFacc`, `Equipo`) V
 --
 -- Indices de la tabla `faccion`
 --
-
 ALTER TABLE `faccion`
   ADD PRIMARY KEY (`IdFacc`);
 
@@ -92,11 +91,10 @@ ALTER TABLE `tripulante`
 --
 
 --
--- AUTO_INCREMENT de la tabla `recetas`
+-- AUTO_INCREMENT de la tabla `tripulante`
 --
-
 ALTER TABLE `tripulante`
-  MODIFY `IdTrip` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `IdTrip` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Restricciones para tablas volcadas
